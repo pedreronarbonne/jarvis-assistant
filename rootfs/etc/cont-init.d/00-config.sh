4c976a7d266ac0ea18d49382c2f4b29a35240bf7
pedreronarbonne <pedrero.narbonne@gmail.com>
2026-01-06 23:06:39 +0100

    Update 00-config.sh

--- rootfs/etc/cont-init.d/00-config.sh
+++ rootfs/etc/cont-init.d/00-config.sh
@@ -6,11 +6,8 @@ if bashio::config.exists 'assistant_id'; then
     ASSISTANT_ID=$(bashio::config 'assistant_id')
     bashio::log.info "Assistant ID: ${ASSISTANT_ID}"
     
-    # Injecter la config dans le HTML
     sed -i "s|conversation.hf_co_unsloth_qwen3_vl_8b_instruct_gguf_q8_0_ai_agent|${ASSISTANT_ID}|g" /var/www/html/index.html
     bashio::log.info "Configuration appliquée avec succès"
 else
-    bashio::log.warning "Aucune configuration trouvée, utilisation des valeurs par défaut"
+    bashio::log.warning "Utilisation des valeurs par défaut"
 fi
-
-bashio::log.info "Configuration terminée"
