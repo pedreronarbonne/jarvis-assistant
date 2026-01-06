d7c0ce5b5e17e69652b4636c11bf5f0790e725ce
pedreronarbonne <pedrero.narbonne@gmail.com>
2026-01-06 23:31:38 +0100

    Update 00-config.sh

--- rootfs/etc/cont-init.d/00-config.sh
+++ rootfs/etc/cont-init.d/00-config.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/with-contenv bash
+set -e
 
 bashio::log.info "Configuration de Jarvis Assistant..."
 
 if bashio::config.exists 'assistant_id'; then
     ASSISTANT_ID=$(bashio::config 'assistant_id')
     bashio::log.info "Assistant ID: ${ASSISTANT_ID}"
-    
+
     # Injecter la config dans le HTML
     sed -i "s|conversation.hf_co_unsloth_qwen3_vl_8b_instruct_gguf_q8_0_ai_agent|${ASSISTANT_ID}|g" /var/www/html/index.html
     bashio::log.info "Configuration appliquée avec succès"
